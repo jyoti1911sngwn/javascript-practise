1abd04e78394ccfb78b9e03e91193976a66f2be1
jyoti sangwan <jyotisangwan@jyotis-Air.lan>
2025-12-18 18:05:18 +0530

    indexof

--- array&string/flatarr.js
+++ array&string/flatarr.js
@@ -1,3 +1,18 @@
 const arr = [[1,[2,[3,[4]]]], 2]
-const newarr = arr.flat(5)
+// const newarr = arr.flat(5)
+// const newarr = arr.flat(Infinity)
+const newarr=[]
+function flatten(arr){
+
+    for ( let i = 0 ; i<arr.length ; i++){
+        if(Array.isArray(arr[i])){
+            flatten(arr[i])
+        }
+        else{
+            newarr.push(arr[i])
+        }
+    }
+}
+
+flatten(arr);
 console.log(newarr)

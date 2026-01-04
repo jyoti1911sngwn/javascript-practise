4a5adc04cc9b4341ad7377ad7be1f534d2c39826
jyoti sangwan <jyotisangwan@jyotis-Air.lan>
2026-01-04 17:58:22 +0530

    leet

--- rotatebyk.js
+++ rotatebyk.js
@@ -1,19 +1,22 @@
 const arr = [1, 2, 3, 4, 4, 5];
 let k = 2;
 
-const n = arr.length;
-k = k % n;
-
-reverse(arr, 0, n - 1);
-reverse(arr, 0, k - 1);
-reverse(arr, k, n - 1);
-
-console.log(arr);
-
-function reverse(a, start, end) {
-  while (start < end) {
-    [a[start], a[end]] = [a[end], a[start]];
-    start++;
-    end--;
+let start = 0
+let end = arr.length-1
+const n = arr.length
+k= k%n
+function reverse (arr, start, end){
+  while(start<end){
+    [arr[start],arr[end]] = [arr[end], arr[start]]
+    start++
+    end--
   }
+  return arr
 }
+
+
+const first = reverse(arr, 0, k-1) 
+const sec = reverse(arr, k, n-1)
+console.log(reverse(arr, start, end))
+
+// console.log(reverse(arr, start, end))

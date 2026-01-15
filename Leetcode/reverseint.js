e9b054d8d5ea0dcbcb9afff62cdf085e8e434dd2
jyoti sangwan <jyotisangwan@jyotis-Air.lan>
2026-01-15 14:42:49 +0530

    leet

--- Leetcode/reverseint.js
+++ Leetcode/reverseint.js
@@ -1,24 +1,21 @@
-
-
-
 /**
  * @param {number} x
  * @return {number}
  */
-var reverse = function(x) {
-    let str = Math.abs(x).toString()
-    let newstr = ""
-    let sign = x<0 ? -1 : 1
-    for(let i = str.length-1 ; i>=0 ; i--){
-        newstr+=str[i]
-    }
- let result = sign * Number(newstr);
+var reverse = function (x) {
+  let str = Math.abs(x).toString();
+  let newstr = "";
+  let sign = x < 0 ? -1 : 1;
+  for (let i = str.length - 1; i >= 0; i--) {
+    newstr += str[i];
+  }
+  let result = sign * Number(newstr);
 
-    if (result < -2147483648 || result > 2147483647) {
-        return 0;
-    }
+  if (result < -2147483648 || result > 2147483647) {
+    return 0;
+  }
 
-    return result;
+  return result;
 };
 
-console.log(reverse(123))
+console.log(reverse(123));
